67f2b8a1a21f64952a7184c22aabd913faa4fc94
Nguyen Trong Phat <phatnt@rakumo.vn>
2020-09-09 15:18:27 +0700

    fix
    fix

--- firstRecurring.playground/Contents.swift
+++ firstRecurring.playground/Contents.swift
@@ -7,14 +7,10 @@ let array3 = [2, 3, 4, 5]
 func firstRecurring(array: [Int]) -> Int? { // O(n)
     var dic: [Int: Int] = [:]
     for index in 0...array.count - 1 { // O(n)
-        dic[array[index]] = 0
-    }
-    for index in 0...array.count - 1 { // O(n)
-        if dic.keys.contains(array[index]) {
-            dic[array[index]]! += 1
-        }
-        if dic[array[index]]! > 1 {
+        if dic[array[index]] != nil {
             return array[index]
+        } else {
+            dic[array[index]] = index
         }
     }
     return nil
